e8b5fd4e27eb108abe96e62511f6fddead93b2de
redBOY <43438363+redBOY@users.noreply.github.com>
2022-08-17 01:12:48 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -1,13 +1,39 @@
 package main.BridgeLabz;
+package com.bridgelabz.tictactoe;
+
+import java.util.Scanner;
 
 public class tictactoe {
-    static char[] board = new char[10];
-    static void createboard(char []board){
-        for(int i =1; i<10; i++){
-            board[i]=' ';
+
+    static char board[] = new char[10];
+    static Scanner scr = new Scanner(System.in);
+    static char playerLetter;
+    static char computerLetter;
+
+    static void createBoard(){
+        for(int i = 1; i < 10; i++){
+            board[i] =' ';
         }
     }
-public static void main(String[] args) {
-    System.out.println("welcome to tictactoe game");
-}
+
+    static void getLetter(){
+        System.out.println("Choose your letter X or O");
+        playerLetter = scr.next().toUpperCase().charAt(0);
+        computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+        System.out.println("Your letter is: "+playerLetter);
+        System.out.println("computer letter is: "+computerLetter);
+    }
+
+    static void showBoard(){
+        System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+        System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+        System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        createBoard();
+        getLetter();
+        showBoard();
+    }
 }
